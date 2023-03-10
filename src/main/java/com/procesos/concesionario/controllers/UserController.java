81e7dee78f5c2bf1c612d56406f454895a7520ac
Camilo Pinzon <capinzonm@ufpso.edu.co>
2023-03-09 21:26:15 -0500

    Actualizacion error

--- src/main/java/com/procesos/concesionario/controllers/UserController.java
+++ src/main/java/com/procesos/concesionario/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.procesos.concesionario.controllers;
 
 import com.procesos.concesionario.models.User;
 import com.procesos.concesionario.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
     private UserService userService;
     @GetMapping(value = "/user/{id}")
     public User getById(@PathVariable(name = "id") Long id){
